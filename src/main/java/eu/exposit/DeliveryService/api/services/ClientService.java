229ff33b50283ca3d553bf029476b4eb99800a49
baddreammello <andrej.czimbalist@inbox.ru>
2021-06-19 01:37:17 +0300

    create: service marker interfaces

---
+++ src/main/java/eu/exposit/DeliveryService/api/services/ClientService.java
@@ -0,0 +1,7 @@
+package eu.exposit.DeliveryService.api.services;
+
+import eu.exposit.DeliveryService.model.Client;
+
+public interface ClientService extends CrudService<Client> {
+
+}
---
+++ src/main/java/eu/exposit/DeliveryService/api/services/ShopService.java
@@ -0,0 +1,8 @@
+package eu.exposit.DeliveryService.api.services;
+
+import eu.exposit.DeliveryService.model.Shop;
+
+public interface ShopService extends CrudService<Shop> {
+
+
+}
