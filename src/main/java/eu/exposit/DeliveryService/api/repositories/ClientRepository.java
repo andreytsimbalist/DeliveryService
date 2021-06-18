bdedc2a953458c3e234d071dbe9733797ac4f0e5
baddreammello <andrej.czimbalist@inbox.ru>
2021-06-19 01:36:58 +0300

    create: repository marker interfaces

---
+++ src/main/java/eu/exposit/DeliveryService/api/repositories/ClientRepository.java
@@ -0,0 +1,8 @@
+package eu.exposit.DeliveryService.api.repositories;
+
+import eu.exposit.DeliveryService.model.Client;
+
+public interface ClientRepository extends CrudRepository<Client> {
+
+
+}
---
+++ src/main/java/eu/exposit/DeliveryService/api/repositories/ShopRepository.java
@@ -0,0 +1,8 @@
+package eu.exposit.DeliveryService.api.repositories;
+
+import eu.exposit.DeliveryService.model.Shop;
+
+public interface ShopRepository extends CrudRepository<Shop> {
+
+
+}
