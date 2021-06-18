b4ae12585fc7686ba063a3e09df7ac4184bd0b3d
baddreammello <andrej.czimbalist@inbox.ru>
2021-06-19 01:28:04 +0300

    create: class AbstractDatabase

---
+++ src/main/java/eu/exposit/DeliveryService/DataStorage/AbstractDatabase.java
@@ -0,0 +1,23 @@
+package eu.exposit.DeliveryService.DataStorage;
+
+import eu.exposit.DeliveryService.model.BaseEntity;
+import eu.exposit.DeliveryService.model.Shop;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public abstract class AbstractDatabase<T extends BaseEntity> {
+
+    private List<T> entities;
+    private Long idSequence;
+
+    public T create(T entity) {
+        entity.setId(idSequence++);
+        entities.add(entity);
+        return entity;
+    }
+
+}
