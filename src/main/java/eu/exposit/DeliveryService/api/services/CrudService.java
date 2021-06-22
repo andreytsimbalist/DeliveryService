f3efbb14b20553c8a5a829f2119a1d974bb07aad
baddreammello <andrej.czimbalist@inbox.ru>
2021-06-22 16:21:44 +0300

    add: throws in create() method create: get(), delete() methods

--- src/main/java/eu/exposit/DeliveryService/api/services/CrudService.java
+++ src/main/java/eu/exposit/DeliveryService/api/services/CrudService.java
@@ -1,19 +1,24 @@
 package eu.exposit.DeliveryService.api.services;
 
 import eu.exposit.DeliveryService.exceptions.NoRecordException;
+import eu.exposit.DeliveryService.exceptions.RecordAlreadyExistsException;
 import eu.exposit.DeliveryService.model.BaseEntity;
 
 import java.util.List;
 
 public interface CrudService<T extends BaseEntity> {
 
-    T create(T entity);
+    T create(T entity) throws RecordAlreadyExistsException;
+
+    T get(T entity) throws NoRecordException;
 
     T getById(Long id) throws NoRecordException;
 
     List<T> getAll();
 
     T update(T entity);
+
+    void delete(T entity) throws NoRecordException;
 
     void deleteById(Long id) throws NoRecordException;
 
