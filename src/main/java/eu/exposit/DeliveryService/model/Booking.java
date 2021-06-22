eec6e2ca396d7e9babd01daed46757d1bd5894c0
baddreammello <andrej.czimbalist@inbox.ru>
2021-06-22 16:00:02 +0300

    create: booking entity

--- src/main/java/eu/exposit/DeliveryService/model/Booking.java
+++ src/main/java/eu/exposit/DeliveryService/model/Booking.java
@@ -1,6 +1,36 @@
 package eu.exposit.DeliveryService.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
 public class Booking extends BaseEntity {
 
+    private Client client;
+    private List<Shop> shops;
+    private float total;
 
+    public Booking() {
+        shops = new ArrayList<>();
+    }
+
+    private String shopsToString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Shop shop : shops) {
+            stringBuilder.append(shop);
+        }
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "\nИнофрмация заказа:" +
+                "\nКлиент" + client.toString() +
+                "\nМагазины" + shopsToString() +
+                "\nИтого: " + total;
+    }
 }
