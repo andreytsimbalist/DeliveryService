8140f1090b723271d688f8900660b067eba6a85d
baddreammello <andrej.czimbalist@inbox.ru>
2021-06-22 16:03:42 +0300

    create: console util

---
+++ src/main/java/eu/exposit/DeliveryService/utils/ConsoleUtil.java
@@ -0,0 +1,12 @@
+package eu.exposit.DeliveryService.utils;
+
+import lombok.Getter;
+
+import java.util.Scanner;
+
+public class ConsoleUtil {
+
+    @Getter
+    private static final Scanner scanner = new Scanner(System.in);
+
+}
