87fa90f3b499aa8b3bd30797ae96f225da778f5d
baddreammello <andrej.czimbalist@inbox.ru>
2021-06-22 16:11:58 +0300

    delete: price and capacity lines create: copy and empty constructor, toString

--- src/main/java/eu/exposit/DeliveryService/model/Product.java
+++ src/main/java/eu/exposit/DeliveryService/model/Product.java
@@ -4,14 +4,38 @@ import eu.exposit.DeliveryService.model.enums.Category;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class Product extends BaseEntity {
 
+    private String name;
     private List<Category> categories;
-    private Double price;
-    private Integer capacity;
+
+    public Product() {
+        categories = new ArrayList<>();
+    }
+
+    public Product(Product product){
+        name = product.getName();
+        categories = product.getCategories();
+        id = product.getId();
+    }
+
+    private String categoriesToString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Category category : categories) {
+            stringBuilder.append("\n— ").append(category.getDescription());
+        }
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "\nНазвание товара: " + name +
+                "\nКатегории: " + categoriesToString();
+    }
 
 }
