cc767d6b7b748fb71db036159d68c06129de4607
baddreammello <andrej.czimbalist@inbox.ru>
2021-06-22 16:08:02 +0300

    delete: unnecessary imports add: sortByKey(SortKey sortKey) method

--- src/main/java/eu/exposit/DeliveryService/api/services/ShopService.java
+++ src/main/java/eu/exposit/DeliveryService/api/services/ShopService.java
@@ -1,8 +1,13 @@
 package eu.exposit.DeliveryService.api.services;
 
 import eu.exposit.DeliveryService.model.Shop;
+import eu.exposit.DeliveryService.model.Stock;
+import eu.exposit.DeliveryService.model.enums.SortKey;
+
+import java.util.List;
 
 public interface ShopService extends CrudService<Shop> {
 
+    List<Stock> sortByKey(SortKey sortKey);
 
 }
