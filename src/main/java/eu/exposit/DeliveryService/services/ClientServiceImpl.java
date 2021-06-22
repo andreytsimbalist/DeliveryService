94c05ea17b0b6963fc04619dc1e368786be82254
baddreammello <andrej.czimbalist@inbox.ru>
2021-06-22 16:22:25 +0300

    add: Override create method

--- src/main/java/eu/exposit/DeliveryService/services/ClientServiceImpl.java
+++ src/main/java/eu/exposit/DeliveryService/services/ClientServiceImpl.java
@@ -1,6 +1,7 @@
 package eu.exposit.DeliveryService.services;
 
 import eu.exposit.DeliveryService.api.services.ClientService;
+import eu.exposit.DeliveryService.exceptions.RecordAlreadyExistsException;
 import eu.exposit.DeliveryService.model.Client;
 import eu.exposit.DeliveryService.repositories.ClientRepositoryImpl;
 
@@ -17,6 +18,18 @@ public class ClientServiceImpl extends CrudServiceImpl<Client> implements Client
             instance = new ClientServiceImpl();
         }
         return instance;
+    }
+
+    @Override
+    public Client create(Client client) throws RecordAlreadyExistsException {
+        for (Client temp : crudRepository.getAll()) {
+            if (temp.getName().equals(client.getName()) &&
+                    temp.getSurname().equals(client.getSurname()) &&
+                    temp.getNumber().equals(client.getNumber())) {
+                throw new RecordAlreadyExistsException();
+            }
+        }
+        return super.create(client);
     }
 
 }
