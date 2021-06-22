e137587722d822bdf7c9ae41f13b588d6e96e20c
baddreammello <andrej.czimbalist@inbox.ru>
2021-06-22 16:18:59 +0300

    create: marker service interfaces

---
+++ src/main/java/eu/exposit/DeliveryService/api/services/BookingService.java
@@ -0,0 +1,9 @@
+package eu.exposit.DeliveryService.api.services;
+
+import eu.exposit.DeliveryService.model.Booking;
+
+public interface BookingService extends CrudService<Booking> {
+
+
+
+}
---
+++ src/main/java/eu/exposit/DeliveryService/api/services/ProductService.java
@@ -0,0 +1,6 @@
+package eu.exposit.DeliveryService.api.services;
+
+import eu.exposit.DeliveryService.model.Product;
+
+public interface ProductService extends CrudService<Product> {
+}
