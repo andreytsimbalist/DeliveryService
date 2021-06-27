8c4529aa00363adb7d7d62575b46cbd08655e32c
baddreammello <andrej.czimbalist@inbox.ru>
2021-06-27 18:58:48 +0300

    change: constructor to annotation

--- src/main/java/eu/exposit/deliveryservice/menu/MenuItem.java
+++ src/main/java/eu/exposit/deliveryservice/menu/MenuItem.java
@@ -1,22 +1,18 @@
 package eu.exposit.deliveryservice.menu;
 
 import eu.exposit.deliveryservice.api.actions.Action;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class MenuItem {
 
     private String text;
     private Action action;
     private Menu submenu;
-
-    public MenuItem(String text, Action action, Menu submenu) {
-        this.text = text;
-        this.action = action;
-        this.submenu = submenu;
-    }
 
     public MenuItem(String text, Action action) {
         this.text = text;
